1a15e32cc3704ad132b4b13ddf2ba9872197a8a4
esbiya <18829040039@163.com>
2021-01-08 13:51:48 +0800

    修改 cookies 策略

--- utils.go
+++ utils.go
@@ -11,7 +11,11 @@ func TransferCookies(_cookies []map[string]interface{}) []*http.Cookie {
 	for _, cookie := range _cookies {
 		var _cookie http.Cookie
 		_ = mapstructure.Decode(cookie, &_cookie)
-		_cookie.Expires = time.Unix(cookie["Expire"].(int64), 0)
+		if _, ok := cookie["Expire"]; ok {
+			_cookie.Expires = time.Unix(cookie["Expire"].(int64), 0)
+		} else {
+			_cookie.Expires = time.Now()
+		}
 		cookies = append(cookies, &_cookie)
 	}
 	return cookies
